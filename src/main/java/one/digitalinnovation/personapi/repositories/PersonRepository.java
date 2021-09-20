d00d42530bcac53442636c58cba0b00e517dec40
Maria Luiza Gonçalves de Moura <86756987+mlumoura@users.noreply.github.com>
2021-09-20 13:15:36 -0300

    Insere Mapper

--- src/main/java/one/digitalinnovation/personapi/repositories/PersonRepository.java
+++ src/main/java/one/digitalinnovation/personapi/repositories/PersonRepository.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.repositories;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,25 +1,36 @@
 package one.digitalinnovation.personapi.service;
 
+import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedperson = personRepository.save(person);
+    public MessageResponseDTO createPerson(@Valid PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson =  personRepository.save(personToSave);
+        return createMessageResponse(savedPerson.getId(), "Created person with ID ");
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
         return MessageResponseDTO
                 .builder()
-                .message("Create person with ID " + savedperson.getId())
+                .message(message + id)
                 .build();
     }
 }
